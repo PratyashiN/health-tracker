d3492c96b6d862edc22b6095ccf6bd39955910e5
Pratyashi Nath <pratyashin@gmai.com>
2026-04-08 23:54:40 +0530

    latest changes

--- backend/app.py
+++ backend/app.py
@@ -7,16 +7,10 @@ app = Flask(__name__)
 CORS(app)
 
 create_tables()
-
 app.register_blueprint(routes_bp)
 
 @app.route("/")
 def home():
     return "Backend running!"
 
-if __name__ == "__main__":
-    import os
-
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port)
+print("APP STARTED SUCCESSFULLY")
